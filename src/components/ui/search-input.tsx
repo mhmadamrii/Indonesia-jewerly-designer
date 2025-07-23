07e2c2cd93e29438d4f7e478c92e7bf33173db93
mhmadamrii <interceptorghost4@gmail.com>
2025-07-23 16:49:41 +0700

    fjdk;a

--- src/components/ui/search-input.tsx
+++ src/components/ui/search-input.tsx
@@ -12,16 +12,16 @@ const SearchInput = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <div
         className={cn(
-          "border-input ring-offset-background focus-within:ring-ring flex h-10 items-center rounded-md border pl-3 text-sm focus-within:ring-1 focus-within:ring-offset-2",
+          "border-input ring-offset-background focus-within:ring-ring relative flex h-10 items-center rounded-md border bg-transparent text-sm focus-within:ring-1 focus-within:ring-offset-2",
           className,
         )}
       >
-        <SearchIcon className="h-[16px] w-[16px]" />
+        <SearchIcon className="text-muted-foreground absolute left-3 h-[16px] w-[16px]" />
         <Input
           {...props}
           type="search"
           ref={ref}
-          className="placeholder:text-muted-foreground w-full border-none p-2 focus-visible:border-none focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+          className="placeholder:text-muted-foreground relative w-full border-none p-2 pl-9 focus-visible:border-none focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
         />
       </div>
     );
