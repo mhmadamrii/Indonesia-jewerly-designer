36aead3f3be570920befd82eb2f38fd238c06a46
mhmadamrii <interceptorghost4@gmail.com>
2025-10-31 14:25:14 +0700

    [FIX](navigating): fast navigation mode activated

--- src/components/role-redirect-provider.tsx
+++ src/components/role-redirect-provider.tsx
@@ -1,16 +1,19 @@
+import { useRouterState } from "@tanstack/react-router";
 import { Loader } from "lucide-react";
 import { useRoleStore } from "~/lib/store/role.store";
 
 export function RoleRedirectProvider({ children }: { children: React.ReactNode }) {
+  const isLoading = useRouterState({ select: (s) => s.isLoading });
   const { isRoleChanging } = useRoleStore();
 
   return (
     <>
-      {isRoleChanging && (
-        <div className="fixed top-0 left-0 z-50 flex h-full w-full cursor-not-allowed flex-col items-center justify-center bg-black/50">
-          <Loader className="animate-spin" />
-        </div>
-      )}
+      {isRoleChanging ||
+        (isLoading && (
+          <div className="fixed top-0 left-0 z-50 flex h-full w-full cursor-not-allowed flex-col items-center justify-center bg-black/50">
+            <Loader className="animate-spin" />
+          </div>
+        ))}
       {children}
     </>
   );
